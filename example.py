e7faec3fcc1219844db63eae6c5682e336eb2cd1
Matt Hersant <matt_hersant@yahoo.com>
2015-03-06 03:02:46 -0800

    minor changes

--- example.py
+++ example.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python
+
 import sqlreports
 
-rpt = sqlreports.sql({      \
-    'ENGINE' : 'mysql',     \
-    'HOST'   : 'localhost', \
-    'USER'   : 'root',      \
-    'NAME'   : 'matt',      \
+rpt = sqlreports.sql({        \
+    'ENGINE'   : 'mysql',     \
+    'HOST'     : 'localhost', \
+    'USER'     : 'dbuser',    \
+    'PASSWD'   : 'dbpass',    \
+    'NAME'     : 'matt',      \
 })
 
 results = rpt.runQuery('select id, name, age from example')
 
+# multi workbook spreadsheets will be represented as a 3 deminsional array
+# workbook1, workbook2
 xls = sqlreports.spreadsheet(results)
 xls.column_names = rpt.column_names
 xls.createSpreadsheet()
